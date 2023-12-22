335015939hi/dumbos-platform_hardware_interfaces

Add a new utility class wifi_hidl_test_utils_1_6 for the Wifi VTS tests.
Main method is getBridgedWifiApIface_1_6, which
allows us to configure the chip based on the
Bridged AP IfaceConcurrencyType, rather than
the AP IfaceType like in getBridgedWifiApIface_1_5.

Implementations were largely copied from
wifi_hidl_test_utils and wifi_hidl_test_utils_1_5.

Bug: 283402709
Test: atest VtsHalHostapdTargetTest
       # tested on a Bluejay device that supports
       # the AddAccessPointWithDualBandConfig test
Change-Id: I2571876149fc14c8de02e1cec9934dd052cada5c

wifi/hostapd/aidl/vts/functional/VtsHalHostapdTargetTest.cpp
@@ -29,6 +29,7 @@
 #include <hostapd_hidl_test_utils.h>
 #include <wifi_hidl_test_utils.h>
 #include <wifi_hidl_test_utils_1_5.h>
+#include <wifi_hidl_test_utils_1_6.h>
 
 using aidl::android::hardware::wifi::hostapd::BandMask;
 using aidl::android::hardware::wifi::hostapd::BnHostapdCallback;
@@ -101,7 +102,7 @@ class HostapdAidl : public testing::TestWithParam<std::string> {
     std::string setupApIfaceAndGetName(bool isBridged) {
         android::sp<::android::hardware::wifi::V1_0::IWifiApIface> wifi_ap_iface;
         if (isBridged) {
-            wifi_ap_iface = getBridgedWifiApIface_1_5(wifiInstanceName);
+            wifi_ap_iface = getBridgedWifiApIface_1_6(wifiInstanceName);
         } else {
             wifi_ap_iface = getWifiApIface_1_5(wifiInstanceName);
         }

LineVall/linevall_hardware_interfaces

wifi/hostapd/aidl/vts/functional/VtsHalHostapdTargetTest.cpp
@@ -30,6 +30,7 @@
 #include <hostapd_hidl_test_utils.h>
 #include <wifi_hidl_test_utils.h>
 #include <wifi_hidl_test_utils_1_5.h>
+#include <wifi_hidl_test_utils_1_6.h>
 
 #include "wifi_aidl_test_utils.h"
 
@@ -143,7 +144,7 @@ class HostapdAidl : public testing::TestWithParam<std::string> {
     std::string setupApIfaceAndGetNameHidl(bool isBridged) {
         android::sp<::android::hardware::wifi::V1_0::IWifiApIface> wifi_ap_iface;
         if (isBridged) {
-            wifi_ap_iface = getBridgedWifiApIface_1_5(wifiHidlInstanceName);
+            wifi_ap_iface = getBridgedWifiApIface_1_6(wifiHidlInstanceName);
         } else {
             wifi_ap_iface = getWifiApIface_1_5(wifiHidlInstanceName);
         }